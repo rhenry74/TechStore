814641cbe8dafc422d90e8366da2a35256881e64
Robert Henry <rhenry74@hotmail.com>
2023-12-28 21:40:48 -0500

    mess, fluent componests intellisense not working api and adtra still at net 6

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddFluentUIComponents();
 
 builder.Services.AddScoped<IPersistence, AzureBlobPersistence>();
 
