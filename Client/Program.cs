d6abc5d6cc6236f6f3c68b358b99583a1a62585b
Robert Henry <rhenry74@hotmail.com>
2023-12-29 11:57:11 -0500

    trying to get bootstarp to work

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
-using Microsoft.FluentUI.AspNetCore.Components;
+using BlazorStrap;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("app");
@@ -9,8 +9,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddFluentUIComponents();
-
 builder.Services.AddScoped<IPersistence, AzureBlobPersistence>();
+
+builder.Services.AddBlazorStrap();
 
 await builder.Build().RunAsync();
